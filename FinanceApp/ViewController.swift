9c4656c7eba53072f634c9e48729a87ee2e839da
Andrey Talanchuk <talanchuk@hotmail.com>
2021-02-19 17:09:37 +0300

    Initial Commit

---
+++ FinanceApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FinanceApp
+//
+//  Created by Андрей Таланчук on 19.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
